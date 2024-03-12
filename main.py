548208c26ebbcbc37101c3e40d91b39d30f7a128
Alwaysuttam <itsuttammail@gmail.com>
2024-03-12 14:50:36 +0530

    Data ingestion done

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.spam_classifier import logger
+from src.spam_classifier.pipeline.s1_Data_Ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
