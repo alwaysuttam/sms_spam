50afcf6f3d1a1e6281d5f29c91c61a25073271f7
Alwaysuttam <itsuttammail@gmail.com>
2024-03-13 10:58:18 +0530

    Data validation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.spam_classifier import logger
 from src.spam_classifier.pipeline.s1_Data_Ingestion import DataIngestionTrainingPipeline
+from src.spam_classifier.pipeline.s2_Data_Validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,3 +12,20 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
